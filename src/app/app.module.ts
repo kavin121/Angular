88200ab4d38fc5d7e66cfdc9cc19c4816a0b65e1
kavin121 <kavindot121@gmail.com>
2020-01-25 14:05:11 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule,MatToolbarModule,MatIconModule,MatMenuModule,MatCardModule,MatListModule,MatSidenavModule} from '@angular/material';
+import { OneComponent } from './one/one.component';
 
+const routes: Routes = [
+  {path:'one',component:OneComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OneComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,MatButtonModule,MatSidenavModule,
+    MatToolbarModule,MatIconModule,MatMenuModule,MatCardModule,MatListModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
